af135c63eca3b59aea69bc7acf21e6f5342079cb
FoyArthur <20FoyA06@stu.smuhsd.org>
2019-01-25 09:35:02 -0800

    commit

--- FracCalc/src/fracCalc/Fraction.java
+++ FracCalc/src/fracCalc/Fraction.java
@@ -10,5 +10,35 @@ public class Fraction {
 		}else {
 			whole = 0;
 		}
+		if(operand.indexOf("/") != -1 && operand.indexOf("_") != -1){
+			numerator = Integer.parseInt(operand.substring(operand.indexOf("_") + 1, operand.indexOf("/")));
+			denominator = Integer.parseInt(operand.substring(operand.indexOf("/") + 1));
+		}else if(operand.indexOf("/") != -1) {
+			numerator = Integer.parseInt(operand.substring(0, operand.indexOf("/")));
+			denominator = Integer.parseInt(operand.substring(operand.indexOf("/") + 1));
+		}else {
+			numerator = 0;
+			denominator = 1;
+		}
+		toImproperFraction();
+	}
+	
+	public Fraction() {
+		whole = 0;
+		numerator = 0;
+		denominator = 1;
+	}
+	
+	public void toImproperFraction() {
+		numerator = denominator * whole + numerator;
+	}
+	public void doMath(String operator, Fraction fraction) {
+		if(operator.equals("+")) {
+			numerator = (numerator * fraction.denominator) + (fraction.numerator * denominator);
+			denominator = denominator * fraction.denominator;
+		}else if(operator.equals("-")) {
+			numerator = (numerator * fraction.denominator) - (fraction.numerator * denominator);
+			denominator = denominator * fraction.denominator;
+		}else if()
 	}
 }
